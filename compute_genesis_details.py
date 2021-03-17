c729b849e785f519982e89388193942a9b2d1e9b
protolambda <proto@protolambda.com>
2021-03-17 01:46:56 +0100

    update genesis details script

--- compute_genesis_details.py
+++ compute_genesis_details.py
@@ -4,10 +4,22 @@ from eth2spec.phase0.spec import *
 with open('genesis.ssz', 'rb') as f:
     genesis_state = BeaconState.deserialize(f, os.stat('genesis.ssz').st_size)
 
+updated_header = genesis_state.latest_block_header.copy()
+updated_header.state_root = genesis_state.hash_tree_root()
+
+assert genesis_state.latest_block_header.state_root == Bytes32()
+
 print(f"""
 genesis_time: {genesis_state.genesis_time}
 genesis_state_root: 0x{genesis_state.hash_tree_root().hex()}
-genesis_block_root: 0x{genesis_state.latest_block_header.hash_tree_root().hex()}
+genesis_latest_block_header:
+  slot: {genesis_state.latest_block_header.slot}
+  proposer_index: {genesis_state.latest_block_header.proposer_index}
+  parent_root: 0x{genesis_state.latest_block_header.parent_root.hex()}
+  state_root: 0x{genesis_state.latest_block_header.state_root.hex()}
+  body_root: 0x{genesis_state.latest_block_header.body_root.hex()}
+genesis_block_root_no_state_root: 0x{genesis_state.latest_block_header.hash_tree_root().hex()}
+genesis_block_root_updated_state_root: 0x{updated_header.hash_tree_root().hex()}
 genesis_validators_root: 0x{genesis_state.validators.hash_tree_root().hex()}
 genesis_validators_count: {genesis_state.validators.length()}
 genesis_active_validators_count: {len(get_active_validator_indices(genesis_state, Epoch(0)))}
@@ -22,3 +34,4 @@ genesis_fork_version: 0x{genesis_state.fork.current_version.hex()}
 genesis_fork_digest: 0x{compute_fork_digest(genesis_state.fork.current_version, genesis_state.validators.hash_tree_root()).hex()}
 pre_genesis_fork_digest: 0x{compute_fork_digest(genesis_state.fork.current_version, Root()).hex()}
 """)
+
