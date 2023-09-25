34ec9f2c58050d5c2686afcbe70d8743f25f4e78
parithosh <parithosh.jayanthi@ethereum.org>
2023-09-25 19:18:47 +0200

    adding verkle genesis command

--- go.mod
+++ go.mod
@@ -3,70 +3,88 @@ module github.com/protolambda/eth2-testnet-genesis
 go 1.19
 
 require (
-	github.com/ethereum/go-ethereum v1.11.5
-	github.com/herumi/bls-eth-go-binary v1.29.1
-	github.com/holiman/uint256 v1.2.2
+	github.com/ethereum/go-ethereum v1.13.1
+	github.com/herumi/bls-eth-go-binary v1.32.0
+	github.com/holiman/uint256 v1.2.3
 	github.com/protolambda/ask v0.1.3
 	github.com/protolambda/zrnt v0.30.0
-	github.com/protolambda/ztyp v0.2.2
+	github.com/protolambda/ztyp v0.2.3-0.20230210160528-a0236251c773
 	github.com/tyler-smith/go-bip39 v1.1.0
-	github.com/wealdtech/go-eth2-util v1.8.1
-	golang.org/x/sync v0.1.0
+	github.com/wealdtech/go-eth2-util v1.8.2
+	golang.org/x/sync v0.3.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/DataDog/zstd v1.5.2 // indirect
+	github.com/DataDog/zstd v1.5.5 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/VictoriaMetrics/fastcache v1.12.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bits-and-blooms/bitset v1.9.0 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/cockroachdb/errors v1.9.1 // indirect
+	github.com/cockroachdb/errors v1.11.1 // indirect
 	github.com/cockroachdb/logtags v0.0.0-20230118201751-21c54148d20b // indirect
-	github.com/cockroachdb/pebble v0.0.0-20230404150825-93eff0a72e22 // indirect
-	github.com/cockroachdb/redact v1.1.3 // indirect
-	github.com/deckarep/golang-set/v2 v2.3.0 // indirect
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/cockroachdb/pebble v0.0.0-20230922144958-86593692e09f // indirect
+	github.com/cockroachdb/redact v1.1.5 // indirect
+	github.com/cockroachdb/tokenbucket v0.0.0-20230807174530-cc333fc44b06 // indirect
+	github.com/consensys/bavard v0.1.13 // indirect
+	github.com/consensys/gnark-crypto v0.11.3-0.20230906172141-49815a21349a // indirect
+	github.com/crate-crypto/go-ipa v0.0.0-20230914135612-d1b03fcb8e58 // indirect
+	github.com/crate-crypto/go-kzg-4844 v0.4.0 // indirect
+	github.com/deckarep/golang-set/v2 v2.3.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/ethereum/c-kzg-4844 v0.3.1 // indirect
 	github.com/ferranbt/fastssz v0.1.3 // indirect
-	github.com/getsentry/sentry-go v0.20.0 // indirect
-	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/gballet/go-verkle v0.1.1-0.20230921190644-1a60d228f7b2 // indirect
+	github.com/getsentry/sentry-go v0.24.1 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/gofrs/flock v0.8.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/golang/snappy v0.0.4 // indirect
+	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/holiman/bloomfilter/v2 v2.0.3 // indirect
 	github.com/kilic/bls12-381 v0.1.0 // indirect
-	github.com/klauspost/compress v1.16.3 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
-	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/minio/sha256-simd v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/prometheus/client_golang v1.14.0 // indirect
-	github.com/prometheus/client_model v0.3.0 // indirect
-	github.com/prometheus/common v0.42.0 // indirect
-	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/prometheus/client_golang v1.16.0 // indirect
+	github.com/prometheus/client_model v0.4.0 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/protolambda/bls12-381-util v0.0.0-20220416220906-d8552aa452c7 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
-	github.com/rogpeppe/go-internal v1.10.0 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/supranational/blst v0.3.11 // indirect
 	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7 // indirect
-	github.com/tklauser/go-sysconf v0.3.11 // indirect
-	github.com/tklauser/numcpus v0.6.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/wealdtech/go-bytesutil v1.2.1 // indirect
-	github.com/wealdtech/go-eth2-types/v2 v2.8.1 // indirect
-	github.com/yusufpapurcu/wmi v1.2.2 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
-	golang.org/x/exp v0.0.0-20230321023759-10a507213a29 // indirect
-	golang.org/x/sys v0.7.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	github.com/wealdtech/go-eth2-types/v2 v2.8.2 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
 )
+
+replace github.com/protolambda/zrnt => github.com/gballet/zrnt v0.10.2-0.20230922082911-972190796be5
+
+replace github.com/ethereum/go-ethereum => github.com/gballet/go-ethereum v1.10.24-0.20230925152730-3e6b0058fc1d
---
+++ verkle.go
@@ -0,0 +1,201 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"github.com/protolambda/zrnt/eth2/beacon/verkle"
+	"math/big"
+	"os"
+	"time"
+
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/types"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/holiman/uint256"
+	"github.com/protolambda/zrnt/eth2"
+	"github.com/protolambda/zrnt/eth2/beacon/common"
+	"github.com/protolambda/zrnt/eth2/configs"
+	"github.com/protolambda/ztyp/codec"
+	"github.com/protolambda/ztyp/tree"
+	"github.com/protolambda/ztyp/view"
+)
+
+type VerkleGenesisCmd struct {
+	configs.SpecOptions `ask:"."`
+	Eth1Config          string `ask:"--eth1-config" help:"Path to config JSON for eth1. No transition yet if empty."`
+
+	Eth1BlockHash      common.Root      `ask:"--eth1-block" help:"If not transitioned: Eth1 block hash to put into state."`
+	Eth1BlockTimestamp common.Timestamp `ask:"--eth1-timestamp" help:"If not transitioned: Eth1 block timestamp"`
+
+	MnemonicsSrcFilePath  string `ask:"--mnemonics" help:"File with YAML of key sources"`
+	ValidatorsSrcFilePath string `ask:"--additional-validators" help:"File with list of additional validators"`
+	StateOutputPath       string `ask:"--state-output" help:"Output path for state file"`
+	TranchesDir           string `ask:"--tranches-dir" help:"Directory to dump lists of pubkeys of each tranche in"`
+
+	EthWithdrawalAddress common.Eth1Address `ask:"--eth1-withdrawal-address" help:"Eth1 Withdrawal to set for the genesis validator set"`
+	ShadowForkEth1RPC    string             `ask:"--shadow-fork-eth1-rpc" help:"Fetch the Eth1 block from the eth1 node for the shadow fork"`
+}
+
+func (g *VerkleGenesisCmd) Help() string {
+	return "Create genesis state for verkle beacon chain, from execution-layer and consensus-layer configs"
+}
+
+func (g *VerkleGenesisCmd) Default() {
+	g.SpecOptions.Default()
+	g.Eth1Config = "engine_genesis.json"
+
+	g.Eth1BlockHash = common.Root{}
+	g.Eth1BlockTimestamp = common.Timestamp(time.Now().Unix())
+
+	g.MnemonicsSrcFilePath = "mnemonics.yaml"
+	g.ValidatorsSrcFilePath = ""
+	g.StateOutputPath = "genesis.ssz"
+	g.TranchesDir = "tranches"
+	g.ShadowForkEth1RPC = ""
+}
+
+func (g *VerkleGenesisCmd) Run(ctx context.Context, args ...string) error {
+	fmt.Printf("zrnt version: %s\n", eth2.VERSION)
+
+	spec, err := g.SpecOptions.Spec()
+	if err != nil {
+		return err
+	}
+
+	var eth1BlockHash common.Root
+	var beaconGenesisTimestamp common.Timestamp
+	var execHeader *verkle.ExecutionPayloadHeader
+	var eth1Block *types.Block
+	var prevRandaoMix [32]byte
+	var TxRoot [32]byte
+	var eth1Genesis *core.Genesis
+
+	// Load the Eth1 block from the Eth1 genesis config
+	if g.Eth1Config != "" {
+		eth1Genesis, err = loadEth1GenesisConf(g.Eth1Config)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Load the genesis timestamp from the CL config, this is better in terms of compatibility for shadowforks
+	if spec.MIN_GENESIS_TIME != 0 {
+		fmt.Println("Using CL MIN_GENESIS_TIME for genesis timestamp")
+
+		// Set beaconchain genesis timestamp based on config genesis timestamp
+		beaconGenesisTimestamp = spec.MIN_GENESIS_TIME
+	} else {
+		beaconGenesisTimestamp = g.Eth1BlockTimestamp
+	}
+
+	if g.ShadowForkEth1RPC != "" {
+		client, err := ethclient.Dial(g.ShadowForkEth1RPC)
+		if err != nil {
+			return fmt.Errorf("A fatal error occurred creating the ETH client %s", err)
+		}
+
+		// Get the latest block
+		blockNumberUint64, err := client.BlockNumber(ctx)
+		blockNumberBigint := new(big.Int).SetUint64(blockNumberUint64)
+		resultBlock, err := client.BlockByNumber(ctx, blockNumberBigint)
+		if err != nil {
+			return fmt.Errorf("A fatal error occurred getting the ETH block %s", err)
+		}
+
+		// Set the eth1Block value for use later
+		eth1Block = resultBlock
+
+		// Convert and set the difficulty as the prevRandao field
+		prevRandaoMix = bigIntToBytes32(eth1Block.Difficulty())
+
+		// Copy the txRoot from the block
+		copy(TxRoot[:], eth1Block.TxHash().Bytes())
+
+	} else if g.Eth1Config != "" {
+
+		// Generate genesis block from the loaded config
+		eth1Block = eth1Genesis.ToBlock()
+
+		// Set as default values
+		prevRandaoMix = common.Bytes32{}
+		TxRoot = common.PayloadTransactionsType(spec).DefaultNode().MerkleRoot(tree.GetHashFn())
+
+	} else {
+		fmt.Println("no eth1 config found, using eth1 block hash and timestamp, with empty ExecutionPayloadHeader (no PoW->PoS transition yet in execution layer)")
+		eth1BlockHash = g.Eth1BlockHash
+		execHeader = &verkle.ExecutionPayloadHeader{}
+	}
+
+	eth1BlockHash = common.Root(eth1Block.Hash())
+
+	extra := eth1Block.Extra()
+	if len(extra) > common.MAX_EXTRA_DATA_BYTES {
+		return fmt.Errorf("extra data is %d bytes, max is %d", len(extra), common.MAX_EXTRA_DATA_BYTES)
+	}
+
+	baseFee, _ := uint256.FromBig(eth1Block.BaseFee())
+
+	execHeader = &verkle.ExecutionPayloadHeader{
+		ParentHash:    common.Root(eth1Block.ParentHash()),
+		FeeRecipient:  common.Eth1Address(eth1Block.Coinbase()),
+		StateRoot:     common.Bytes32(eth1Block.Root()),
+		ReceiptsRoot:  common.Bytes32(eth1Block.ReceiptHash()),
+		LogsBloom:     common.LogsBloom(eth1Block.Bloom()),
+		PrevRandao:    prevRandaoMix,
+		BlockNumber:   view.Uint64View(eth1Block.NumberU64()),
+		GasLimit:      view.Uint64View(eth1Block.GasLimit()),
+		GasUsed:       view.Uint64View(eth1Block.GasUsed()),
+		Timestamp:     common.Timestamp(eth1Block.Time()),
+		ExtraData:     extra,
+		BaseFeePerGas: view.Uint256View(*baseFee),
+		BlockHash:     eth1BlockHash,
+
+		TransactionsRoot:     TxRoot,
+		ExecutionWitnessRoot: verkle.ExecutionWitnessType.DefaultNode().MerkleRoot(tree.GetHashFn()),
+	}
+
+	if err := os.MkdirAll(g.TranchesDir, 0777); err != nil {
+		return err
+	}
+
+	validators, err := loadValidatorKeys(spec, g.MnemonicsSrcFilePath, g.ValidatorsSrcFilePath, g.TranchesDir, g.EthWithdrawalAddress)
+	if err != nil {
+		return err
+	}
+
+	if uint64(len(validators)) < uint64(spec.MIN_GENESIS_ACTIVE_VALIDATOR_COUNT) {
+		fmt.Printf("WARNING: not enough validators for genesis. Key sources sum up to %d total. But need %d.\n", len(validators), spec.MIN_GENESIS_ACTIVE_VALIDATOR_COUNT)
+	}
+
+	state := verkle.NewBeaconStateView(spec)
+	if err := setupState(spec, state, beaconGenesisTimestamp, eth1BlockHash, validators); err != nil {
+		return err
+	}
+
+	if err := state.SetLatestExecutionPayloadHeader(execHeader); err != nil {
+		return err
+	}
+
+	t, err := state.GenesisTime()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("eth2 genesis at %d + %d = %d  (%s)\n", beaconGenesisTimestamp, spec.GENESIS_DELAY, t, time.Unix(int64(t), 0).String())
+
+	fmt.Println("done preparing state, serializing SSZ now...")
+	f, err := os.OpenFile(g.StateOutputPath, os.O_CREATE|os.O_WRONLY, 0777)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	buf := bufio.NewWriter(f)
+	defer buf.Flush()
+	w := codec.NewEncodingWriter(f)
+	if err := state.Serialize(w); err != nil {
+		return err
+	}
+	fmt.Println("done!")
+	return nil
+}
